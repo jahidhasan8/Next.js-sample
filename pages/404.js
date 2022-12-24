13fa2bd21f9e800928230a9d07570c271ab4d706
jahidhasan8 <jahidhasan1932@gmail.com>
2022-12-24 17:45:21 +0600

    second commit

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+const Error = () => {
+    return (
+        <div>
+            <Image src="/error.png" width={1400} height={50}></Image>
+            <Link href="/">
+            <button className='btn btn-primary'>Back Home</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Error;
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,42 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Banner from '../Components/Banner/Banner'
 import Footer from '../Components/Footer/Footer'
+import Link from 'next/link'
 
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <div>
+    <div className='text-center'>
       <Banner></Banner>
+      <div>
+        {
+          posts.map(post => <div key={post.id} className="card my-10 mx-10 bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">{post?.title}</h2>
+
+             <p>{post?.body}</p>
+            </div>
+          </div>)
+        }
+
+        <Link href="/posts">
+        <button className="btn btn-primary">See More</button>
+        </Link>
+      </div>
       <Footer></Footer>
     </div>
   )
+}
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+  const data = await res.json()
+
+  return {
+    props: {
+      posts: data
+    }
+  }
 }
